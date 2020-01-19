bbec2c5cd84f3d5b433d2b3e550d92f2fa58c1dd
kwanghyunMoon <mgh3326@naver.com>
2020-01-19 20:57:52 +0900

    3_예제 도메인 모델

---
+++ src/main/java/study/querydsl/entity/Team.java
@@ -0,0 +1,26 @@
+package study.querydsl.entity;
+
+import lombok.*;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@ToString(of = {"id", "name"})
+public class Team {
+  @OneToMany(mappedBy = "team")
+  List<Member> members = new ArrayList<>();
+  @Id
+  @GeneratedValue
+  @Column(name = "team_id")
+  private Long id;
+  private String name;
+
+  public Team(String name) {
+    this.name = name;
+  }
+}
