1062b03ca5b9f774ba1d0f5ea92c294518b3310a
kwanghyunMoon <mgh3326@naver.com>
2020-02-02 14:54:34 +0900

    5_동적 쿼리 - Where 다중 파라미터 사용

--- src/test/java/study/querydsl/QuerydslBasicTest.java
+++ src/test/java/study/querydsl/QuerydslBasicTest.java
@@ -1,13 +1,16 @@
 package study.querydsl;
 
+import com.querydsl.core.BooleanBuilder;
 import com.querydsl.core.QueryResults;
 import com.querydsl.core.Tuple;
 import com.querydsl.core.types.ExpressionUtils;
 import com.querydsl.core.types.Projections;
+import com.querydsl.core.types.dsl.BooleanExpression;
 import com.querydsl.core.types.dsl.CaseBuilder;
 import com.querydsl.core.types.dsl.Expressions;
 import com.querydsl.jpa.JPAExpressions;
 import com.querydsl.jpa.impl.JPAQueryFactory;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -26,7 +29,7 @@ import javax.persistence.PersistenceContext;
 import javax.persistence.PersistenceUnit;
 import java.util.List;
 
-import static com.querydsl.jpa.JPAExpressions.*;
+import static com.querydsl.jpa.JPAExpressions.select;
 import static org.assertj.core.api.Assertions.assertThat;
 import static study.querydsl.entity.QMember.member;
 import static study.querydsl.entity.QTeam.team;
@@ -479,6 +482,54 @@ public class QuerydslBasicTest {
     for (MemberDto memberDto : result) {
       System.out.println("memberDto = " + memberDto);
     }
+  }
 
+  @Test
+  public void 동적쿼리_BooleanBuilder() throws Exception {
+    String usernameParam = "member1";
+    Integer ageParam = 10;
+    List<Member> result = searchMember1(usernameParam, ageParam);
+    Assertions.assertThat(result.size()).isEqualTo(1);
+  }
+
+  private List<Member> searchMember1(String usernameCond, Integer ageCond) {
+    BooleanBuilder builder = new BooleanBuilder();
+    if (usernameCond != null) {
+      builder.and(member.username.eq(usernameCond));
+    }
+    if (ageCond != null) {
+      builder.and(member.age.eq(ageCond));
+    }
+    return queryFactory
+            .selectFrom(member)
+            .where(builder)
+            .fetch();
+  }
+
+  @Test
+  public void 동적쿼리_WhereParam() throws Exception {
+    String usernameParam = "member1";
+    Integer ageParam = 10;
+    List<Member> result = searchMember2(usernameParam, ageParam);
+    Assertions.assertThat(result.size()).isEqualTo(1);
+  }
+
+  private List<Member> searchMember2(String usernameCond, Integer ageCond) {
+    return queryFactory
+            .selectFrom(member)
+            .where(usernameEq(usernameCond), ageEq(ageCond))
+            .fetch();
+  }
+
+  private BooleanExpression usernameEq(String usernameCond) {
+    return usernameCond != null ? member.username.eq(usernameCond) : null;
+  }
+
+  private BooleanExpression ageEq(Integer ageCond) {
+    return ageCond != null ? member.age.eq(ageCond) : null;
+  }
+
+  private BooleanExpression allEq(String usernameCond, Integer ageCond) {
+    return usernameEq(usernameCond).and(ageEq(ageCond));
   }
 }
